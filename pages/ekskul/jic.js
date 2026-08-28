2c3834134a902ca02257dc146a2265863223cf0e
r/fatur?! <mhmdfaturrhmn18@gmail.com>
2026-08-28 19:29:37 +0700

    Add Japanese Indonesian Club (JIC) data

---
+++ pages/ekskul/jic.js
@@ -0,0 +1,17 @@
+window.EKSKUL_DATA = window.EKSKUL_DATA || [];
+
+window.EKSKUL_DATA.push({
+  id: "jic",
+  name: "Japanese Indonesian Club (JIC)",
+  badge: "Kebudayaan & Bahasa",
+  image: "assets/banner/ekskul/jic.jpg",
+  shortDesc: "Komunitas pecinta budaya Jepang dan Indonesia. Mempelajari bahasa, kebudayaan, seni, hingga pertukaran budaya.",
+  fullDesc: "Japanese Indonesian Club (JIC) SMK Turah 01 Bogor adalah wadah bagi siswa yang tertarik mempelajari kebudayaan, bahasa, serta seni tradisional maupun modern dari Jepang dan Indonesia. Ekskul ini bertujuan memperluas wawasan internasional siswa.",
+  kegiatan: [
+    "Pembelajaran Bahasa Jepang Dasar (Hiragana, Katakana, Kanji)",
+    "Workshop Cosplay, Anime, & Pop Culture",
+    "Belajar Seni Kebudayaan Tradisional (Origami, Shodo)",
+    "Penyelenggaraan Festival Budaya Sekolah (Bunkasai)"
+  ],
+  jadwal: "Setiap Rabu, 15.30 - 17.00 WIB"
+});
