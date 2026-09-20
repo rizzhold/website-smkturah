b2b37a9ee4e563ee10eb1531c051db735c20cf8e
stickman <mhmdfaturrhmn18@gmail.com>
2026-09-20 18:00:08 +0700

    Add review data for Andi Firmansyah

---
+++ content/ulasan/andi.js
@@ -0,0 +1,9 @@
+window.ULASAN_DATA = window.ULASAN_DATA || [];
+
+window.ULASAN_DATA.push({
+    nama: "Andi Firmansyah",
+    email: "andi.firmansyah@gmail.com",
+    rating: 5,
+    pesan: "Alumni TKJ 2023 di sini. Sekarang udah kerja di perusahaan IT. Materi yang diajarin bener-bener kepake di dunia kerja. Recommended!",
+    tanggal: "2025-07-15"
+});
