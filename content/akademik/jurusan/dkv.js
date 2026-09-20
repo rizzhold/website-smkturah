8dc17a45c7ec6eba92e1a27ef81c9fc7f211366e
stickman <mhmdfaturrhmn18@gmail.com>
2026-09-20 17:52:47 +0700

    Add DKV program data to JURUSAN_DATA
    Added data for Desain Komunikasi Visual (DKV) program including descriptions, photos, materials, and job prospects.

---
+++ content/akademik/jurusan/dkv.js
@@ -0,0 +1,33 @@
+if (typeof JURUSAN_DATA === 'undefined') {
+  var JURUSAN_DATA = [];
+}
+
+JURUSAN_DATA.push({
+  id: "dkv",
+  name: "Desain Komunikasi Visual (DKV)",
+  badge: "Seni & Industri Kreatif",
+  hide: false,
+  image: "assets/banner/jurusan/dkv.jpg",
+  shortDesc: "Mempelajari seni grafis digital, ilustrasi, branding produk, fotografi industri, hingga desain UI/UX aplikasi.",
+  fullDesc: "Desain Komunikasi Visual (DKV) SMK Turah 01 Bogor melatih kreativitas siswa dalam menyampaikan pesan visual secara efektif. Siswa diajarkan menguasai software desain standar industri, fotografi, videografi, serta konsep branding visual.",
+  photos: [
+    { src: "https://picsum.photos/id/250/1200/800", tag: "Banner" },
+    { src: "https://picsum.photos/id/1062/1200/800", tag: "Studio DKV" },
+    { src: "https://picsum.photos/id/1084/1200/800", tag: "Praktik Fotografi" },
+    { src: "https://picsum.photos/id/1074/1200/800", tag: "Pameran Karya" }
+  ],
+  materi: [
+    "Desain Grafis Komersial (Adobe Photoshop & Illustrator)",
+    "Ilustrasi Digital & Vektor (CorelDRAW & Procreate)",
+    "Fotografi Produk & Studio Lighting",
+    "Branding, Packaging Design & Tipografi",
+    "Dasar Desain UI/UX & Animasi 2D"
+  ],
+  prospekKerja: [
+    "Graphic Designer & Illustrator",
+    "UI/UX Designer Junior",
+    "Social Media Content Creator",
+    "Commercial Photographer",
+    "Creative Director Assistant"
+  ]
+});
