1196733b98009d37c6f28bcd0db4ed3d8df45998
stickman <mhmdfaturrhmn18@gmail.com>
2026-09-20 17:57:00 +0700

    Add Angga Revara to GURU_DATA

---
+++ content/guru/angga.js
@@ -0,0 +1,12 @@
+if (typeof GURU_DATA === 'undefined') {
+  var GURU_DATA = [];
+}
+
+GURU_DATA.push({
+  id: "angga",
+  nama: "Angga Revara",
+  gelar: "S.Pd.",
+  jabatan: "Guru Coding & AI",
+  keterangan: "Mengajar pemrograman, logika coding, dan dasar-dasar Artificial Intelligence. Membimbing siswa menciptakan proyek digital inovatif dari nol hingga rilis.",
+  foto: "assets/images/guru/angga.jpg"
+});
