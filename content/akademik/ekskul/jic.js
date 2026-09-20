b74af44b9cc496e87d0f0138dc57d811c8d61459
stickman <mhmdfaturrhmn18@gmail.com>
2026-09-20 17:28:35 +0700

    Add Japanese Indonesian Club (JIC) details

---
+++ content/akademik/ekskul/jic.js
@@ -0,0 +1,13 @@
+window.EKSKUL_DETAIL_DATA = window.EKSKUL_DETAIL_DATA || [];
+
+window.EKSKUL_DETAIL_DATA.push({
+    id: 'jic',
+    number: '01',
+    title: 'Japanese Indonesian Club (JIC)',
+    description: 'Komunitas pecinta budaya Jepang dan Indonesia. Mempelajari bahasa, kebudayaan, seni, hingga pertukaran budaya. Cocok buat kamu yang suka anime, cosplay, atau ingin belajar bahasa Jepang dari dasar.',
+    photos: [
+        { src: 'https://picsum.photos/id/1011/800/500', tag: 'Budaya' },
+        { src: 'https://picsum.photos/id/1024/800/500', tag: 'Bahasa' },
+        { src: 'https://picsum.photos/id/1025/800/500', tag: 'Festival' }
+    ]
+});
